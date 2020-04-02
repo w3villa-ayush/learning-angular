fca21f3d42f0eaf3e8da5172466bba6c903e9c8f
ayush <ayush.jain@w3villa.com>
2020-04-02 10:55:39 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
-import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { ViewComponent } from './view/view.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ParentComponent } from './parent/parent.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'buy-product',component: ParentComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'home',component:HomeComponent},
+  {path:'view',component:ViewComponent},
+  {path:'header',component:HeaderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
+import { MessageService } from './app-service/message.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from  '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+import { Card1Component } from './card1/card1.component';
+import { Card2Component } from './card2/card2.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
+import { Comp4Component } from './comp4/comp4.component';
+import { ViewComponent } from './view/view.component';
+import { Comp5Component } from './comp5/comp5.component';
+import { TestDirectiveDirective } from './app-directive/test-directive.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    ChildComponent,
+    Card1Component,
+    Card2Component,
+    ContactComponent,
+    HomeComponent,
+    Comp1Component,
+    Comp2Component,
+    Comp3Component,
+    Comp4Component,
+    ViewComponent,
+    Comp5Component,
+    TestDirectiveDirective,
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
